23379157d76fa60f8f589c46fff47dbbbdf33039
liv-dias <10737709@mackenzista.com.br>
2026-03-12 12:42:31 +0000

    mudanças minimas

--- Torre.java
+++ Torre.java
@@ -1,11 +1,14 @@
-public class Pilha <T> {
+public class Torre <T> {
     // atributos
     private Object[] elementos;
     private int topo;
     private int tamanho;
     // construtores
-    public Pilha(int tamanho){
+    public Torre(int tamanho){
         this.tamanho = tamanho;
         this.elementos = new Object[tamanho];
         this.topo = -1;
+    }
+    public boolean isEmpty(){
+        return this.topo == -1;
     }
